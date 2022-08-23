ffb7fbe9377fd475201df9286ddb9cbdb9c0ca28
gimsan <gimsan@172.30.65.202>
2022-08-23 21:10:00 +0900

    first commit

---
+++ src/main/java/brokurly/project/backoffice/BrokurlyAdminApplication.java
@@ -0,0 +1,14 @@
+package brokurly.project.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokurlyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokurlyAdminApplication.class, args);
+	}
+
+}
+ 
---
+++ src/test/java/brokurly/project/backoffice/BrokurlyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package brokurly.project.backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokurlyAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
