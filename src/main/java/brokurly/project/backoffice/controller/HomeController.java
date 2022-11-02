922e11a54c1db4fb6f375dab682349d2e55fc084
suchan <aud248@naver.com>
2022-11-02 11:14:12 +0900

    refactor: ModelAndView 제거

--- src/main/java/brokurly/project/backoffice/controller/HomeController.java
+++ src/main/java/brokurly/project/backoffice/controller/HomeController.java
@@ -2,18 +2,12 @@ package brokurly.project.backoffice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller // ResponseBody 필요없음
 public class HomeController {
-//	@GetMapping("/member/show")
-//	public ModelAndView show() {
-//		ModelAndView mv = new ModelAndView();
-//		mv.setViewName("member/show");
-//		return mv;
-//	}
-//	public String indexPage() {
-//		return "member/show";
-//	}
-	
+
+	@GetMapping("/member/show")
+	public String show() {
+		return "/member/show";
+	}
 }
