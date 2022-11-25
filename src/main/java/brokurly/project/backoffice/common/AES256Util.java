937a031718a9d2165ea61f49001a780a2b8cdca4
K-Saaan <111870436+K-Saaan@users.noreply.github.com>
2022-11-25 20:26:27 +0900

    암복호화 AES256Utill 추가 /MemberController logger 추

---
+++ src/main/java/brokurly/project/backoffice/common/AES256Util.java
@@ -0,0 +1,77 @@
+package brokurly.project.backoffice.common;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+
+public class AES256Util {
+
+	@Value("${spring.aesKey}")
+	static String key;
+	
+	public static byte[] ivBytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	
+	
+	/* 암호화
+	 * @param str
+	 * @return str
+	 * @throws java.io.UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
+	*/
+	public static String enCode(String str) throws UnsupportedEncodingException, NoSuchAlgorithmException, 
+	NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException,	
+	IllegalBlockSizeException, BadPaddingException {
+		
+		String secretKey = key.substring(0, 32);
+		byte[] textBytes = str.getBytes("UTF-8");
+		AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
+		Cipher cipher = null;
+		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+		return Base64.encodeBase64String(cipher.doFinal(textBytes));
+	}
+	
+	
+	/* 복호화
+	 * @param str
+	 * @return str
+	 * @throws java.io.UnsupportedEncodingException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
+	*/
+	public static String deCode(String str) throws UnsupportedEncodingException, NoSuchAlgorithmException, 
+	NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException,	
+	IllegalBlockSizeException, BadPaddingException {
+		
+		String secretKey = key.substring(0, 32);
+		byte[] textBytes = Base64.decodeBase64(str);
+		AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes("UTF-8"), "AES");
+		Cipher cipher = null;
+		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
+		return new String(cipher.doFinal(textBytes), "UTF-8");
+	}
+}
--- src/main/java/brokurly/project/backoffice/controller/member/MemberController.java
+++ src/main/java/brokurly/project/backoffice/controller/member/MemberController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor // final 객체를 Constructor Injection 해줌. Autowired 필요없음
 @RequestMapping("/member")
 public class MemberController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private final MemberRepository memberRepository;
 	private final ProductRepository productRepository;
 	
