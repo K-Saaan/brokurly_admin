7668493e99b9b82319c73249771137f0084293a8
Meocintosh <kimdh7776@naver.com>
2023-03-01 11:10:42 +0900

    commit test

--- src/main/resources/static/js/product/product.js
+++ src/main/resources/static/js/product/product.js
@@ -462,7 +462,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			alert("조회된 데이터가 없습니다.");
 		}
 	});
-	
+	// 3.1 commit test
 	// 메인그리드 클릭했을때 해당 데이터의 세부정보를 서브그리드에 보여주기위함
 	function gridCellClicked(){
 		gridView.onCellClicked = function(grid, clickData){
