a40532d43f6c397e3b20792171ff0cc72968e1d9
Meocintosh <kimdh7776@naver.com>
2023-07-03 17:14:40 +0900

    쿠폰 등록상품 사용미사용 처리 시 update문 한번만 나가게 처리

--- src/main/java/brokurly/project/backoffice/repository/product/CouponDtlRepository.java
+++ src/main/java/brokurly/project/backoffice/repository/product/CouponDtlRepository.java
@@ -18,11 +18,11 @@ public interface CouponDtlRepository extends JpaRepository<CouponDtlEntity, Coup
     @Query("select new brokurly.project.backoffice.dto.product.CouponDtlDto(c.cpnCode, c.pdCode, c.useYn, c.regId, c.regDate, c.chgrId, c.chgrDate, p.pdNm) from CouponDtlEntity c join ProductEntity p on c.pdCode = p.pdCode where c.cpnCode = :cpnCode")
     List<CouponDtlDto> showCpnPdInfo(@Param("cpnCode") String cpnCode);
 
-    @Query(value = "select count(*) from pd.cpn_dtl c where CPN_CODE = :cpnCode and PD_CODE = :pdCode", nativeQuery = true)
-    int showCntCpnPd(@Param("cpnCode") String cpnCode, @Param("pdCode") String pdCode);
+//    @Query(value = "select count(*) from pd.cpn_dtl c where CPN_CODE = :cpnCode and PD_CODE = :pdCode", nativeQuery = true)
+//    int showCntCpnPd(@Param("cpnCode") String cpnCode, @Param("pdCode") String pdCode);
 
     @Transactional
     @Modifying
-    @Query(value = "update pd.cpn_dtl set use_yn = :useYn where CPN_CODE = :cpnCode and PD_CODE = :pdCode", nativeQuery = true)
-    void updateUseY(@Param("cpnCode") String cpnCode, @Param("pdCode") String pdCode, @Param("useYn") String useYn);
+    @Query(value = "update pd.cpn_dtl set use_yn = :useYn where CPN_CODE = :cpnCode and PD_CODE in (:pdCode)", nativeQuery = true)
+    void updateUseY(@Param("cpnCode") String cpnCode, @Param("pdCode") List<String> pdCode, @Param("useYn") String useYn);
 }
